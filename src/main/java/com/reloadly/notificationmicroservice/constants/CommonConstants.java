0cad17f8474de2a79f74bdfb9eb07b8e795e5129
danielunogwuatos <daniel.unogwu.external@atos.net>
2021-03-18 03:08:02 +0100

    Created endpoint for sending emails.

--- src/main/java/com/reloadly/notificationmicroservice/constants/CommonConstants.java
+++ src/main/java/com/reloadly/notificationmicroservice/constants/CommonConstants.java
@@ -2,6 +2,4 @@ package com.reloadly.notificationmicroservice.constants;
 
 public interface CommonConstants {
     String API_VERSION = "/api/v1/";
-    String TRANSACTION_EMAIL_TEMPLATE = "transaction";
-    String SUBSCRIPTION_SUBJECT = "Subscription Receipt";
 }

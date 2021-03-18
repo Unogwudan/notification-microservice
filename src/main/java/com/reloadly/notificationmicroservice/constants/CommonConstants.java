d7bde4a245280d0f67c718dca213250ad915feab
danielunogwuatos <daniel.unogwu.external@atos.net>
2021-03-18 03:07:17 +0100

    Added Constants

---
+++ src/main/java/com/reloadly/notificationmicroservice/constants/CommonConstants.java
@@ -0,0 +1,7 @@
+package com.reloadly.notificationmicroservice.constants;
+
+public interface CommonConstants {
+    String API_VERSION = "/api/v1/";
+    String TRANSACTION_EMAIL_TEMPLATE = "transaction";
+    String SUBSCRIPTION_SUBJECT = "Subscription Receipt";
+}
